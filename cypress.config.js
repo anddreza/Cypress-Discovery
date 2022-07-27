62ce03428b5c22acf6e1da252bfbf022ec0a8332
Andreza Silva <dedezasilva@live.com>
2022-07-27 13:44:33 -0300

    ajuste de tamanho da tela

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,11 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+        
     },
+    viewportWidth: 1440, 
+    viewportHeight: 990,
   },
 });
+
+
