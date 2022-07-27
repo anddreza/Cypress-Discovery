b53d80b64f11aef660a4880df4d43d8be5dbd9d0
Andreza Silva <dedezasilva@live.com>
2022-07-27 14:58:41 -0300

    Manipulando tanto as fixture quanto as Array function, ainda não tinha conhecimento desta

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -1,49 +1,38 @@
 import signup from '../pages/SignUpPage'
 
-describe('Cadastro', ()=> {
+describe('Cadastro', () => {
+
+  beforeEach(function () {
+    cy.fixture('deliver').then((d) => {
+      this.deliver = d
+    })
+  })
 
   //var signup = new SignupPage() //fica generico para todos os testes
 
 
   //before(function() {
- //   cy.log("Tudo aqui é executado uma única vez ANTES de TODOS os casos de testes")
- // })
+  //   cy.log("Tudo aqui é executado uma única vez ANTES de TODOS os casos de testes")
+  // })
 
- // beforeEach(function (){
- //   cy.log("Tudo aqui é executado sempre ANTES de CADA caso de teste")
-//  })
+  // beforeEach(function (){
+  //   cy.log("Tudo aqui é executado sempre ANTES de CADA caso de teste")
+  //  })
 
-//  after(function(){
- //   cy.log("Tudo aqui é executado uma única vez DEPOIS de TODOS os casos de testes")
- // })
+  //  after(function(){
+  //   cy.log("Tudo aqui é executado uma única vez DEPOIS de TODOS os casos de testes")
+  // })
 
- // afterEach(function (){
- //   cy.log("Tudo aqui é executado sempre DEPOIS de CADA caso de teste")
- // })
+  // afterEach(function (){
+  //   cy.log("Tudo aqui é executado sempre DEPOIS de CADA caso de teste")
+  // })
 
-  it('Usuário deve se tornar um entregador', ()=> {
-   
-    var deliver = {
-      name: 'Andreza Silva',
-      cpf: '01301374517',
-      email: 'andreza@gmail.com', 
-      whatsapp: '9299999999',
-      address: {
-        postalcode: '04534011',
-        street: 'Rua Joaquim Floriano',
-        number: '1000',
-        details: 'Ap 142',
-        district: 'Itaim Bibi',
-        city_state: 'São Paulo/SP'
-      }, 
-      delivery_method: 'Moto',
-      cnh: 'cnh-digital.jpg'
-    }
+  it('Usuário deve se tornar um entregador', function () {
 
-  //  var signup = new SignupPage()
+    //  var signup = new SignupPage()
 
     signup.go()
-    signup.fillForm(deliver)
+    signup.fillForm(this.deliver.signup)
     signup.submit()
 
     const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
@@ -51,27 +40,9 @@ describe('Cadastro', ()=> {
 
   })
 
-  it('CPF incorreto', ()=> {
-
-    var deliver = {
-      name: 'Andreza Silva',
-      cpf: '812777940AA',
-      email: 'andreza@gmail.com', 
-      whatsapp: '9299999999',
-      address: {
-        postalcode: '04534011',
-        street: 'Rua Joaquim Floriano',
-        number: '1000',
-        details: 'Ap 142',
-        district: 'Itaim Bibi',
-        city_state: 'São Paulo/SP'
-      }, 
-      delivery_method: 'Moto',
-      cnh: 'cnh-digital.jpg'
-    }
-
+  it('CPF incorreto', function () {
     signup.go()
-    signup.fillForm(deliver)
+    signup.fillForm(this.deliver.cpf_inv)
     signup.submit()
     signup.alertMessageShouldBe('Oops! CPF inválido')
 
